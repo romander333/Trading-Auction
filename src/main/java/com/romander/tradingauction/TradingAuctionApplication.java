41a72d2fdecac0321de141be9fb901088bb6bbf6
Luchko Roman <romander09@gmail.com>
2025-09-15 14:13:35 +0300

    first commit

---
+++ src/main/java/com/romander/tradingauction/TradingAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.romander.tradingauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romander/tradingauction/TradingAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romander.tradingauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
